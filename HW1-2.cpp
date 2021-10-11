0ec5a359739be45602f39ef7a6a9c374fe97d890
CHEN-houng <56420181+CHEN-houng@users.noreply.github.com>
2021-10-11 13:45:45 +0800

    Preliminary framework

---
+++ HW1-2.cpp
@@ -0,0 +1,22 @@
+﻿#define _AFXDLL
+#include <iostream>
+#include <afx.h>
+using namespace std;
+
+//
+
+INT FibonacciSeries( INT nInputNumber )
+{
+   if( nInputNumber == 1 or nInputNumber == 2)
+        return 1;
+    if ( nInputNumber >= 3 )
+        return FibonacciSeries(nInputNumber - 1) + FibonacciSeries(nInputNumber - 2);
+}
+
+INT main( void )
+{
+    for (int i = 1; i < 21; i++)
+    {
+        cout << FibonacciSeries(i) << endl;
+    }
+}
