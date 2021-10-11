03f52eb982601be1d319176e52466ca976787d38
CHEN-houng <56420181+CHEN-houng@users.noreply.github.com>
2021-10-11 14:19:30 +0800

    Coding Style Check

--- HW1-2.cpp
+++ HW1-2.cpp
@@ -3,20 +3,21 @@
 #include <afx.h>
 using namespace std;
 
-//
-
 INT FibonacciSeries( INT nInputNumber )
 {
-   if( nInputNumber == 1 or nInputNumber == 2)
-        return 1;
-    if ( nInputNumber >= 3 )
-        return FibonacciSeries(nInputNumber - 1) + FibonacciSeries(nInputNumber - 2);
+   if( nInputNumber == 1 or nInputNumber == 2){
+       return 1;
+   }
+   if (nInputNumber >= 3) {
+       return FibonacciSeries(nInputNumber - 1) + FibonacciSeries(nInputNumber - 2);
+   }    
 }
 
 INT main( void )
 {
-    for (int i = 1; i < 21; i++)
-    {
-        cout << FibonacciSeries(i) << endl;
+    for( INT i = 1; i < 21; i++ ) {
+        cout << "Item:\t" << i << "\t" << FibonacciSeries( i ) << endl;
     }
+    return 0;
 }
+
