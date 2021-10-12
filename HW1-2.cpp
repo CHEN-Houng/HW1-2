da814a471a49f8b5ddfcca622d48b3135c9f719b
CHEN-houng <56420181+CHEN-houng@users.noreply.github.com>
2021-10-12 16:26:10 +0800

    Add files via upload
    Final Version

--- HW1-2.cpp
+++ HW1-2.cpp
@@ -1,23 +1,28 @@
-﻿#define _AFXDLL
 #include <iostream>
 #include <afx.h>
 using namespace std;
 
 INT FibonacciSeries( INT nInputNumber )
 {
-   if( nInputNumber == 1 or nInputNumber == 2){
+// FibonacciSeries main function
+   if( nInputNumber == 1 || nInputNumber == 2 ) {
        return 1;
    }
-   if (nInputNumber >= 3) {
-       return FibonacciSeries(nInputNumber - 1) + FibonacciSeries(nInputNumber - 2);
-   }    
+   else if( nInputNumber >= 3 && nInputNumber <= 46 ) {
+       return FibonacciSeries( nInputNumber - 1 ) + FibonacciSeries( nInputNumber - 2 );
+   }
+   else {
+	   return 0;
+   }
+   
 }
 
 INT main( void )
 {
     for( INT i = 1; i < 21; i++ ) {
+		//  Generate the first 20 digits of the Fibonacci Series
         cout << "Item:\t" << i << "\t" << FibonacciSeries( i ) << endl;
     }
+	system( "PAUSE" );
     return 0;
 }
-
