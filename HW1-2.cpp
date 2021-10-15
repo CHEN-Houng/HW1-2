d51249a68e5a92cf32130840875cf6b5f7440e91
CHEN-houng <56420181+CHEN-houng@users.noreply.github.com>
2021-10-15 17:59:41 +0800

    Review 10/15

--- HW1-2.cpp
+++ HW1-2.cpp
@@ -3,26 +3,46 @@
 using namespace std;
 
 INT FibonacciSeries( INT nInputNumber )
-{
 // FibonacciSeries main function
-   if( nInputNumber == 1 || nInputNumber == 2 ) {
-       return 1;
-   }
-   else if( nInputNumber >= 3 && nInputNumber <= 46 ) {
-       return FibonacciSeries( nInputNumber - 1 ) + FibonacciSeries( nInputNumber - 2 );
-   }
-   else {
-	   return 0;
-   }
-   
+{
+	if( nInputNumber == 1 || nInputNumber == 2 ) {
+		return 1;
+	}
+	else if( nInputNumber >= 3 && nInputNumber <= 46 ) {
+		return FibonacciSeries( nInputNumber - 1 ) + FibonacciSeries( nInputNumber - 2 );
+	}
+	else {
+		return 0;
+	}
+}
+
+void Calculate( INT nFibonacciArray[] )
+// Calculate fibonacciSeries from 1 to 20
+{
+	for( INT i = 0; i < 20; i++ ) {
+		nFibonacciArray[ i ] = FibonacciSeries( i + 1 );
+	}
+}
+
+void Print( INT nFibonacciArray[] )
+// Print finbonacciSeries Results
+{
+	cout << "Item" << "\t" << "Fibonacci Results" << endl;
+	for( INT i = 0; i < 20; i++ ) {
+		cout << i + 1 << "\t" << nFibonacciArray[ i ] << endl;;
+	}
 }
 
 INT main( void )
 {
-    for( INT i = 1; i < 21; i++ ) {
-		//  Generate the first 20 digits of the Fibonacci Series
-        cout << "Item:\t" << i << "\t" << FibonacciSeries( i ) << endl;
-    }
+	INT nFibonacciArray[ 20 ];
+	
+	// Calculate Fibonacci
+	Calculate( nFibonacciArray );
+	
+	// Print Calculate Results
+	Print( nFibonacciArray );
+
 	system( "PAUSE" );
-    return 0;
+	return 0;
 }
